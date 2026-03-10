9c34d0e262fca9b0750fe8c3d9280918ac729e4b
Pushkar Patel <git@thepushkarp.com>
2026-03-10 08:15:05 +0530

    chore: update formula for v0.0.3

--- Formula/mls.rb
+++ Formula/mls.rb
@@ -1,16 +1,16 @@
 class Mls < Formula
   desc "Terminal-native audio/video/image file browser with TUI preview and JSON output"
   homepage "https://github.com/thepushkarp/mls"
-  version "0.0.2"
+  version "0.0.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/thepushkarp/mls/releases/download/v#{version}/mls-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "ebfce47144e7a091baf1861ff98a16d2781a4494060699e0fc4395526f1f3f5e"
+      sha256 "92ca53dce212f3cd9741db216054f604ecd51e3987eec038cdd152ff15487b50"
     elsif Hardware::CPU.intel?
       url "https://github.com/thepushkarp/mls/releases/download/v#{version}/mls-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "7ede35abc687b7a8cb5ddb3ceddc9af6b1b9d9d1adbfcf6b2592c957f9b75ec1"
+      sha256 "2566bbce34f19a03fecf79b2f46c89c58205a0336d77d6767f205206de986815"
     end
   end
 
